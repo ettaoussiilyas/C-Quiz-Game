ee660efcb02cf898338eb8e53f19427cf0fc4dc2
ilyas <ettaoussiilyas0.1@gmail.com>
2024-09-02 22:45:58 +0100

    i just add to arrays one of answers and other for questions

--- quiz.c
+++ quiz.c
@@ -2,7 +2,32 @@
 
 int main(){
 
-    int result , answer;
-
+    int result , answer,i,j;
+        char *questions[10] = {
+        "What is the capital of France?",
+        "Who wrote the book 'To Kill a Mockingbird'?",
+        "What is the largest planet in our solar system?",
+        "What is the smallest planet in our solar system?",
+        "Who invented the telephone?",
+        "What is the name of the first woman to climb Mount Everest?",
+        "Who was the first person to walk on the moon?",
+        "What is the name of the first person to step on Mars?",
+        "Who was the first person to walk on the Moon and back again?",
+        "Who is the current Prime Minister of the United Kingdom?"
+    };
+    char *options[10][4] = {
+    {"Paris01", "London", "Rabat", "Rome"},
+    {"Harper Lee01", "Adam", "Roger", "Sif"},
+    {"Jupiter01", "Mars", "Earth", "Venus"},
+    {"Mercury01", "Mars", "Earth", "Venus"},
+    {"Alexander Graham Bell01", "Thomas Edison", "Nikola Tesla", "Guglielmo Marconi"},
+    {"Junko Tabei01", "Edmund Hillary", "Tenzing Norgay", "Reinhold Messner"},
+    {"Neil Armstrong01", "Buzz Aldrin", "Yuri Gagarin", "Alan Shepard"},
+    {"No one has stepped on Mars01", "Elon Musk", "Neil Armstrong", "Buzz Aldrin"},
+    {"Neil Armstrong01", "Buzz Aldrin", "Michael Collins", "Yuri Gagarin"},
+    {"Rishi Sunak01", "Boris Johnson", "Theresa May", "David Cameron"}
+    };
+    
     return 0;
 }
+
